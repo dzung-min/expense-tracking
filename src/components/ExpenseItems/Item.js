7f60e750e5964d015da9753e08ce2eb2738648e2
Dzung Nguyen <dzung_min@yahoo.com>
2021-05-09 19:26:06 +0700

    add some style to expense date

--- src/components/ExpenseItems/Item.js
+++ src/components/ExpenseItems/Item.js
@@ -1,10 +1,19 @@
+import style from "./Item.module.css";
+
 const Item = ({ expenseData }) => {
   const { date, title, amount } = expenseData;
+  const dateObj = new Date(date);
   return (
     <div>
-      <div>{date}</div>
-      <div>{title}</div>
-      <div>{amount}</div>
+      <div className={style.expenseDate}>
+        <div>{dateObj.toLocaleDateString("en-us", { month: "long" })}</div>
+        <div>{dateObj.getFullYear()}</div>
+        <div className={style.date}>{dateObj.getDate()}</div>
+      </div>
+      <div>
+        <div>{title}</div>
+        <div>{amount}</div>
+      </div>
     </div>
   );
 };
